eb6546f119e6bca5e7a70be74d5b91c53dd47fc7
Ankit <akjain6067.aj@gmail.com>
2021-04-12 17:35:49 +0530

    header fixed

--- src/adchem/header/HeaderFive.jsx
+++ src/adchem/header/HeaderFive.jsx
@@ -1,86 +1,109 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FiX, FiMenu } from "react-icons/fi";
 
-
 class HeaderFive extends Component {
-    constructor(props) {
-        super(props);
-        this.menuTrigger = this.menuTrigger.bind(this);
-        this.CLoseMenuTrigger = this.CLoseMenuTrigger.bind(this);
-        //  this.subMetuTrigger = this.subMetuTrigger.bind(this);
-        window.addEventListener('load', function () {
-            console.log('All assets are loaded')
-        })
+  constructor(props) {
+    super(props);
+    this.menuTrigger = this.menuTrigger.bind(this);
+    this.CLoseMenuTrigger = this.CLoseMenuTrigger.bind(this);
+    //  this.subMetuTrigger = this.subMetuTrigger.bind(this);
+    window.addEventListener("load", function () {
+      console.log("All assets are loaded");
+    });
+  }
+  menuTrigger() {
+    document.querySelector(".header-wrapper").classList.toggle("menu-open");
+  }
+  CLoseMenuTrigger() {
+    document.querySelector(".header-wrapper").classList.remove("menu-open");
+  }
+  render() {
+    var elements = document.querySelectorAll(".has-droupdown > a");
+    for (var i in elements) {
+      if (elements.hasOwnProperty(i)) {
+        elements[i].onclick = function () {
+          this.parentElement
+            .querySelector(".submenu")
+            .classList.toggle("active");
+          this.classList.toggle("open");
+        };
+      }
     }
-    menuTrigger() {
-        document.querySelector('.header-wrapper').classList.toggle('menu-open')
-    }
-    CLoseMenuTrigger() {
-        document.querySelector('.header-wrapper').classList.remove('menu-open');
-    }
-    render() {
-        var elements = document.querySelectorAll('.has-droupdown > a');
-        for (var i in elements) {
-            if (elements.hasOwnProperty(i)) {
-                elements[i].onclick = function () {
-                    this.parentElement.querySelector('.submenu').classList.toggle("active");
-                    this.classList.toggle("open");
-                }
-            }
-        }
 
-        const { color, headerPosition } = this.props;
-        let logoUrl = <img src="/assets/images/logo/adchem-industries-logo.png" alt="ADCHEM industries" />
+    const { color, headerPosition } = this.props;
+    let logoUrl = (
+      <img
+        src="/assets/images/logo/adchem-industries-logo.png"
+        alt="ADCHEM industries"
+      />
+    );
 
-        return (
-            <header className={`header-area formobile-menu ${headerPosition} ${color}`}>
-                <div className="header-wrapper" id="header-wrapper">
-                    <div className="container">
-                        <div className="row align-items-center">
-                            <div className="col-lg-3 col-md-4 col-6">
-                                <div className="header-left">
-                                    <div className="logo">
-                                        <a href="/">
-                                            {logoUrl}
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-9 col-md-8 col-6">
-                                <div className="header-right justify-content-end">
-                                    <nav className="mainmenunav d-lg-block">
-                                        <ul className="mainmenu">
-                                            <li><Link to="/adchem/home">Home</Link>
-                                            </li>
-                                            <li><Link to="/adchem/about-us" >About Us</Link>
-                                            </li>
-                                            <li><Link to="/adchem/products" >Products</Link></li>
-
-                                            <li><Link to="/adchem/clients" >Clients</Link>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                    <div className="header-btn">
-                                        <Link className="rn-btn" to="/adchem/contact-us">
-                                            <span>Contact Us</span>
-                                        </Link>
-                                    </div>
-                                    {/* Start Humberger Menu  */}
-                                    <div className="humberger-menu d-block d-lg-none pl--20">
-                                        <span onClick={this.menuTrigger} className="menutrigger text-white"><FiMenu /></span>
-                                    </div>
-                                    {/* End Humberger Menu  */}
-                                    <div className="close-menu d-block d-lg-none">
-                                        <span onClick={this.CLoseMenuTrigger} className="closeTrigger"><FiX /></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    return (
+      <header
+        className={`header-area formobile-menu ${headerPosition} ${color}`}
+      >
+        <div className="header-wrapper" id="header-wrapper">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-3 col-md-4 col-6">
+                <div className="header-left">
+                  <div className="logo">
+                    <a href="/">{logoUrl}</a>
+                  </div>
                 </div>
-            </header>
-        )
-    }
+              </div>
+              <div className="col-lg-9 col-md-8 col-6">
+                <div className="header-right justify-content-end">
+                  <nav className="mainmenunav d-lg-block">
+                    <ul className="mainmenu">
+                      <li>
+                        <Link to="/adchem/home">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/adchem/about-us">About Us</Link>
+                      </li>
+                      <li>
+                        <Link to="/adchem/products">Products</Link>
+                      </li>
+
+                      <li>
+                        <Link to="/adchem/clients">Clients</Link>
+                      </li>
+
+                      <div className="header-btn" style={{marginTop: "11px"}}>
+                        <Link className="rn-btn" to="/adchem/contact-us">
+                          <span>Contact Us</span>
+                        </Link>
+                      </div>
+                    </ul>
+                  </nav>
+
+                  {/* Start Humberger Menu  */}
+                  <div className="humberger-menu d-block d-lg-none pl--20">
+                    <span
+                      onClick={this.menuTrigger}
+                      className="menutrigger text-white"
+                    >
+                      <FiMenu />
+                    </span>
+                  </div>
+                  {/* End Humberger Menu  */}
+                  <div className="close-menu d-block d-lg-none">
+                    <span
+                      onClick={this.CLoseMenuTrigger}
+                      className="closeTrigger"
+                    >
+                      <FiX />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 export default HeaderFive;
