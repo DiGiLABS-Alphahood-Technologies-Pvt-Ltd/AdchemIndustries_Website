f479822a94a89782959ef86fbff7c6e04f0624d1
Ankit <akjain6067.aj@gmail.com>
2021-06-08 10:13:30 +0530

    added wood adh products slider

--- src/adchem/WoodAdh.jsx
+++ src/adchem/WoodAdh.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Slider from "react-slick";
 import Backdrop from "@material-ui/core/Backdrop";
 import FlexList from "./FlexList";
 import Products from "./Products";
+import { slideSlick } from "./script";
+import { FiCheck } from "react-icons/fi";
 
 const metalImg = (
   <img
@@ -27,6 +30,44 @@ const productList = [
     image: "metal-chem",
     title: "Metal Pre-Treatment Chemicals",
     url: "/products/metal-chem",
+  },
+];
+
+const slideList = [
+  {
+    title: "ADBOND MARINE D3",
+    detail:
+      "IT’S A PREMIUM GRADE PURE PVA BASED ADHESIVE WITH WATERPROFFING PROPERTIES.MARINE D3 HAS LOW VISCOCITY WHICH HELPS FOR ROLLER AND SPRAYABLE APPLICATION.",
+  },
+  {
+    title: "ADBOND MARINE BOND",
+    detail:
+      "IT’S A PREMIUM GRADE PURE PVA BASED ADHESIVE WITH WATERPROFFING PROPERTIES. IT HAS SLIGHTLY HIGH VISCOSITY FOR MANUAL APPLICATION.",
+  },
+  {
+    title: "ADBOND GOLD",
+    detail:
+      "D2 CATEGORY PURE PVA BASED ADHESIVE WITH HIGH SOLID. SLIGHTLY HIGHER VISCOUS THAN FEVICOL SH. IT COMES IN PREMIUM CATEGORY.",
+  },
+  {
+    title: "ADBOND SH",
+    detail:
+      "D2 CATEGORY PURE PVA BASED ADHESIVE WITH HIGH SOLID. SAME VISCOSITY AS FEVICOL SH. IT COMES IN PREMIUM CATEGORY.",
+  },
+  {
+    title: "ADBOND PHI",
+    detail:
+      "D2 CATEGORY PVA BASED ADHESIVE, SOLID CONTAIN LITTLE BIT LESS THAN SH. QUALITY AND PRICE ARE BALANCED HERE WHICH HELPS THE CARPENTER TO WORK WITH PROFIT.",
+  },
+  {
+    title: "ADBOND EXTRA PVA",
+    detail:
+      "D2 CATEGORY PVA BASED ADHESIVE, SOLID CONTAIN LITTLE BIT LESS THAN SH. QUALITY AND PRICE ARE BALANCED HERE WHICH HELPS THE CARPENTER TO WORK WITH PROFIT. IT’S CHEAPER THAN PHI.",
+  },
+  {
+    title: "AD-DOUBLE BOND CAC",
+    detail:
+      "ECONOMIC GRADE STRATCH BASE WOOD WORKING ADHESIVE 26% SOLID CONTAIN.",
   },
 ];
 
@@ -90,11 +131,36 @@ const WoodAdh = (props) => {
           </div>
         </div>
       </div>
-      s
+
       <div className="container mt--50 mb--50 text-center">
         <h2>Advantages</h2>
         <FlexList list={list} />
       </div>
+
+      <div
+        className="slider-wrapper pt--40"
+        style={{ backgroundColor: "#f8f9fc" }}
+      >
+        <h1 className="text-center mb--50">Products</h1>
+        <div className="slider-activation">
+          <Slider className="rn-slick-dot" {...slideSlick}>
+            {slideList.map((item, index) => (
+              <div
+                className={`slide slide-style-3 slider-box-content d-flex align-items-center justify-content-center`}
+                key={index}
+              >
+                <div className="container">
+                  <div className="text-center">
+                    <h4>{item.title}</h4>
+                  </div>
+                  <p style={{textAlign: "center"}}>{item.detail}</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+
       <div className="center">
         <Products title="More Products" list={productList} />
       </div>
