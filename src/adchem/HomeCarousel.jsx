f8207f4aeb5c0fa6a240a7273e1b4fce745d2611
Dhruv1701 <negidhruv1701@gmail.com>
2023-04-01 16:24:56 +0530

    Updated particle.js

--- src/adchem/HomeCarousel.jsx
+++ src/adchem/HomeCarousel.jsx
@@ -69,10 +69,19 @@ const HomeCarousel = (props) => {
         await console.log(container);
     }, []);
 
+
     return (
         <>
             <div className="slider-wrapper">
                 <div className="slider-activation">
+                    <div className="slide-btn">
+                        <Link
+                            to={"/contact-us"}
+                            className="rn-button-style--2 btn-solid"
+                        >
+                            CONTACT US
+                        </Link>
+                    </div>
                     <Slider className="rn-slick-dot" {...slideSlick}>
                         {SlideList.map((value, index) => (
                             <div
@@ -94,18 +103,6 @@ const HomeCarousel = (props) => {
                                                 ) : (
                                                     ""
                                                 )}
-                                                {value.buttonText ? (
-                                                    <div className="slide-btn">
-                                                        <Link
-                                                            to={value.buttonLink}
-                                                            className="rn-button-style--2 btn-solid"
-                                                        >
-                                                            {value.buttonText}
-                                                        </Link>
-                                                    </div>
-                                                ) : (
-                                                    ""
-                                                )}
                                             </div>
                                         </div>
                                     </div>
@@ -113,120 +110,112 @@ const HomeCarousel = (props) => {
                             </div>
                         ))}
                     </Slider>
-                    <Particles className={"particles"}
-                               id="tsparticles"
-                               init={particlesInit}
-                               loaded={particlesLoaded}
-                               options={{
-                                   "particles": {
-                                       "number": {
-                                           "value": 80,
-                                           "density": {
-                                               "enable": true,
-                                               "value_area": 800
-                                           }
-                                       },
-                                       "color": {
-                                           "value": "#ffffff"
-                                       },
-                                       "shape": {
-                                           "type": "circle",
-                                           "stroke": {
-                                               "width": 0,
-                                               "color": "#000000"
-                                           },
-                                           "polygon": {
-                                               "nb_sides": 5
-                                           },
-                                           "image": {
-                                               "src": "img/github.svg",
-                                               "width": 100,
-                                               "height": 100
-                                           }
-                                       },
-                                       "opacity": {
-                                           "value": 0.5,
-                                           "random": false,
-                                           "anim": {
-                                               "enable": false,
-                                               "speed": 1,
-                                               "opacity_min": 0.1,
-                                               "sync": false
-                                           }
-                                       },
-                                       "size": {
-                                           "value": 3,
-                                           "random": true,
-                                           "anim": {
-                                               "enable": false,
-                                               "speed": 40,
-                                               "size_min": 0.1,
-                                               "sync": false
-                                           }
-                                       },
-                                       "line_linked": {
-                                           "enable": true,
-                                           "distance": 150,
-                                           "color": "#ffffff",
-                                           "opacity": 0.4,
-                                           "width": 1
-                                       },
-                                       "move": {
-                                           "enable": true,
-                                           "speed": 6,
-                                           "direction": "none",
-                                           "random": false,
-                                           "straight": false,
-                                           "out_mode": "out",
-                                           "bounce": false,
-                                           "attract": {
-                                               "enable": false,
-                                               "rotateX": 600,
-                                               "rotateY": 1200
-                                           }
-                                       }
-                                   },
-                                   "interactivity": {
-                                       "detect_on": "canvas",
-                                       "events": {
-                                           "onhover": {
-                                               "enable": true,
-                                               "mode": "repulse"
-                                           },
-                                           "onclick": {
-                                               "enable": true,
-                                               "mode": "push"
-                                           },
-                                           "resize": true
-                                       },
-                                       "modes": {
-                                           "grab": {
-                                               "distance": 400,
-                                               "line_linked": {
-                                                   "opacity": 1
-                                               }
-                                           },
-                                           "bubble": {
-                                               "distance": 400,
-                                               "size": 40,
-                                               "duration": 2,
-                                               "opacity": 8,
-                                               "speed": 3
-                                           },
-                                           "repulse": {
-                                               "distance": 200,
-                                               "duration": 0.4
-                                           },
-                                           "push": {
-                                               "particles_nb": 4
-                                           },
-                                           "remove": {
-                                               "particles_nb": 2
-                                           }
-                                       }
-                                   },
-                                   "retina_detect": true,
-                               }}
+                    <Particles
+                        id="tsparticles"
+                        init={particlesInit}
+                        loaded={particlesLoaded}
+                        options={{
+                            "particles": {
+                                "number": {
+                                    "value": 80,
+                                    "density": {
+                                        "enable": true,
+                                        "value_area": 800
+                                    }
+                                },
+                                "color": {
+                                    "value": "#ffffff"
+                                },
+                                "shape": {
+                                    "type": "circle",
+                                    "stroke": {
+                                        "width": 0,
+                                        "color": "#000000"
+                                    },
+                                    "polygon": {
+                                        "nb_sides": 5
+                                    }
+                                },
+                                "opacity": {
+                                    "value": 0.5,
+                                    "random": false,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 1,
+                                        "opacity_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "size": {
+                                    "value": 3,
+                                    "random": true,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 40,
+                                        "size_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "line_linked": {
+                                    "enable": true,
+                                    "distance": 150,
+                                    "color": "#ffffff",
+                                    "opacity": 0.4,
+                                    "width": 1
+                                },
+                                "move": {
+                                    "enable": true,
+                                    "speed": 6,
+                                    "direction": "none",
+                                    "random": false,
+                                    "straight": false,
+                                    "out_mode": "out",
+                                    "bounce": false,
+                                    "attract": {
+                                        "enable": false,
+                                        "rotateX": 600,
+                                        "rotateY": 1200
+                                    }
+                                }
+                            },
+                            "interactivity": {
+                                "detect_on": "canvas",
+                                "events": {
+                                    "onhover": {
+                                        "enable": true,
+                                        "mode": "repulse"
+                                    },
+                                    "resize": true
+                                },
+                                "modes": {
+                                    "grab": {
+                                        "distance": 400,
+                                        "line_linked": {
+                                            "opacity": 1
+                                        }
+                                    },
+                                    "bubble": {
+                                        "distance": 400,
+                                        "size": 40,
+                                        "duration": 2,
+                                        "opacity": 8,
+                                        "speed": 3
+                                    },
+                                    "repulse": {
+                                        "distance": 200,
+                                        "duration": 0.4
+                                    },
+                                    "push": {
+                                        "particles_nb": 4
+                                    },
+                                    "remove": {
+                                        "particles_nb": 2
+                                    }
+                                }
+                            },
+                            "retina_detect": true,
+                            fullScreen: {enable: false},
+                        }}
                     />
                 </div>
             </div>
